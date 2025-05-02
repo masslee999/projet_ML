bfd41a406511de9e5afb7d0856829cb3c764b01c
ahmadounaa <amelless1230@gmail.com>
2025-05-02 10:32:35 +0000

    Amadou cissé

---
+++ nb_pair_ou_impair.py
@@ -0,0 +1,5 @@
+n = int(input("Entrez un nombre de votre choix : "))
+if n%2==0:
+    print("Le nombre que vous avez choisi est pair")
+else:
+    print("Le nombre que vous avez choisi est impair M")
